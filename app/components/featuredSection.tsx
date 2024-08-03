9a5b8246b30e7547d9a68586fe9d101b79e2502a
fktona <adetona.fk@gmail.com>
2024-08-03 09:01:42 -0700

    feat: added  more animations

--- app/components/featuredSection.tsx
+++ app/components/featuredSection.tsx
@@ -27,17 +27,17 @@ const FeaturedSection = () => {
           {FeaturedHostels.map((hostel: Hostel) => (
             <HostelCard key={hostel.id} hostel={hostel} />
           ))}
-           <div className=" relative flex lg:min-w-[250px]    
-             flex-col gap-4 pb-4 bg-white drop-shadow-lg">
+           <div className=" relative flex h-full  lg:min-w-[250px]    
+             flex-col gap-4 pb-4 bg-white drop-shadow-lg min-h-[350px]">
           <div className='absolute w-full h-full bg-black/20 bottom-0 z-10'></div>
-        <div className="absolute h-full group-hover:opacity-75">
+        <div className="absolute  h-full w-full group-hover:opacity-75">
           <Image
             src={'/hero1.jpg'}
             alt={'name'}
             width={500}
             height={350}
             objectFit="contain"
-            className="w-full bottom-0 h-full"
+            className="w-full h-full absolute  "
           />
           </div>
           <div className='absolute pb-4 z-30 space-y-5 bottom-0 text-white px-4 w-full'>
